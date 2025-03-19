10f741e89a17e63642de456cd776e655aaab38ba
Anton Sidorov <antosid@gmail.com>
2025-03-19 11:39:10 +0000

    format

--- test/crux.c
+++ test/crux.c
@@ -6,17 +6,17 @@ int main() {
 	linear_arena_init(&linear_arena, 1024 * 1024);
 
 	LoggerOptions logger_options = {
-                .verbose = 0,
-                .log_level = "DEBUG",
-                .err_log = NULL,
-                .out_log = NULL  
-        };
+		.verbose = 0,
+		.log_level = "DEBUG",
+		.err_log = NULL,
+		.out_log = NULL  
+	};
 
 	Logger *logger = (Logger *)linear_arena_alloc(&linear_arena, sizeof(Logger));
 	logger_init(logger, &logger_options);
 
 	linear_arena_status_update(&linear_arena);
-        info_log(logger, linear_arena.status);      
+	info_log(logger, linear_arena.status);      
 
 	linear_arena_free(&linear_arena);
 	return 0;
