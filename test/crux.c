88ba4ab0ccdbe4741153aa271c584a556f94dca8
Anton Sidorov <antosid@gmail.com>
2025-04-11 08:31:38 +0100

    logger_opaque:

--- test/crux.c
+++ test/crux.c
@@ -10,31 +10,15 @@ const size_t MegaByte = (size_t)(1024 * 1024);
 int main(void) {
 	LinearMemoryArena *linear_arena = linear_arena_init(MegaByte);
 
-	Logger *logger =
-	    (Logger *)linear_arena_alloc(linear_arena, sizeof(Logger));
-	Logger *logger1 = (Logger *)malloc(sizeof(Logger));
-	if (!logger1) {
+	Logger *logger = logger_init(NULL, NULL, "INFO");
+	if (!logger) {
 		perror("Cannot allocate logger!");
 		return -1;
 	}
-	logger_init(logger, NULL, NULL, "INFO");
-	logger_init(logger1, NULL, NULL, "INFO");
-
-	Logger logger2 = {.err_log = NULL, .out_log = NULL, .level = LOG_ERROR};
-	logger_init(&logger2, NULL, NULL, "INFO");
 
 	linear_arena_reset(linear_arena);
 
-	// Cleanup
 	logger_close(logger);
-
-	// info_log(logger1, linear_arena->status);
-	// info_log(&logger2, linear_arena->status);
-
-	logger_close(logger1);
-	logger_close(&logger2);
-
-	free(logger1);
 	linear_arena_free(linear_arena);
 
 	return 0;
